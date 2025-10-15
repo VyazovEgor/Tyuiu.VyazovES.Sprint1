a97940d88ade4b4c5eb684d3609aada1595f7aec
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-10-15 20:20:59 +0500

    push

---
+++ Tyuiu.VyazovES.Sprint1.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+
+namespace Tyuiu.VyazovES.Sprint1.Task1.V23.Lib
+{
+    public class DataService : ISprint1Task1V23
+    {
+        public double Calculate(double x, double a)
+        {
+            double Pi = 3.14;
+            return (x * Pi) / (2 * a);
+
+        }
+
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint1.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.VyazovES.Sprint1.Task1.V23.Lib;
+
+namespace Tyuiu.VyazovES.Sprint1.Task1.V23.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 1;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.57, res);
+
+
+        }
+    }
+}
