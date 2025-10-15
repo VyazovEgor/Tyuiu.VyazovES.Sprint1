04009e33124d3355c186b2e5a1802be08d8b5a09
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-10-16 00:18:41 +0500

    push

--- Tyuiu.VyazovES.Sprint1.Task7.V18.Lib/DataService.cs
+++ Tyuiu.VyazovES.Sprint1.Task7.V18.Lib/DataService.cs
@@ -5,8 +5,8 @@ namespace Tyuiu.VyazovES.Sprint1.Task7.V18.Lib
     {
         public  double Calculate(double x, double y)
         {
-            return (1 + Math.Pow(Math.Sin(x + y), 2)) / (2 + Math.Abs(x - (2 * x) / (1 + Math.Pow(x, 2) * Math.Pow(y, 2)))) + x; ;
-
+            double res = (1 + Math.Pow(Math.Sin(x + y), 2)) / (2 + Math.Abs(x - (2 * x) / (1 + Math.Pow(x, 2) * Math.Pow(y, 2)))) + x; ;
+            return Math.Round(res, 3);
         }
 
     }
