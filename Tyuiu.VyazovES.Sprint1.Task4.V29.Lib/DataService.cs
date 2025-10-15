0efadb1e3d09e99d8a900e02bf5e4cf3bd39d432
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-10-15 23:14:38 +0500

    push

--- Tyuiu.VyazovES.Sprint1.Task4.V29.Lib/DataService.cs
+++ Tyuiu.VyazovES.Sprint1.Task4.V29.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.VyazovES.Sprint1.Task4.V29.Lib
     {
         public double Calculate(double x, double y)
         {
-            double a = (Math.Sqrt(2+Math.Abs(x-2*y))) / 3 * x * y * y ;
+            double a = (Math.Sqrt(2+Math.Abs(x-2*y))) / (3 * x * y * y) ;
 
             return Math.Round(a, 3);
 
