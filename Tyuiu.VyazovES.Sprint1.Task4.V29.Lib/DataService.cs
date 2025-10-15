f5e2e6921c74c5c41c152e0f55dd56d62ea52190
Егор Вязов <Tyuiu.VyazovES@outlook.com>
2025-10-15 23:11:55 +0500

    push

---
+++ Tyuiu.VyazovES.Sprint1.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.VyazovES.Sprint1.Task4.V29.Lib
+{
+    public class DataService : ISprint1Task4V29
+    {
+        public double Calculate(double x, double y)
+        {
+            double a = (Math.Sqrt(2+Math.Abs(x-2*y))) / 3 * x * y * y ;
+
+            return Math.Round(a, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.VyazovES.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VyazovES.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.VyazovES.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 1;
+            double y = 1;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.57, res);
+
+
+        }
+    }
+}
